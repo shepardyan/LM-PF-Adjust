8c1adda8ff0988de5c6871eca5710538b408b6a3
Yunqi Yan <54395770+shepardyan@users.noreply.github.com>
2025-03-26 10:46:31 +0800

    Update Plot

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ if __name__ == "__main__":
     # ppc['bus'][:, VM] = 1.0
 
     x, Fp, Fq, Fq_current = lm_adjust(ppc, verbose=False)
-    plt.plot(Fp, label="Active Subproblem", )
+    plt.plot(Fp, label="Active Subproblem")
     plt.plot(Fq, label="Reactive Subproblem")
     plt.yscale('log')
     plt.legend()
-    plt.savefig("residual.png", dpi=600)
     plt.xlabel("# Iter")
     plt.ylabel("2-Norm of Residual")
+    plt.savefig("residual.png", dpi=600)
     plt.close()
 
     sorted_Fq_index = list(np.argsort(Fq_current)[-11:])
@@ -40,6 +40,8 @@ if __name__ == "__main__":
                 f'{height:.3f}',
                 ha='center', va='bottom')
     plt.tight_layout()
+    plt.xlabel("# Bus")
+    plt.ylabel("2-Norm of Residual")
     plt.savefig("reactive.png", dpi=600)
     plt.close()
 
